a4b372a899fb2cedd8f8523a4a0a53f3de99030e
Lesther <Lesther993@gmail.com>
2018-03-07 23:55:05 -0400

    activity sequence

--- actionRecognition/ActionRecognitionModule.py
+++ actionRecognition/ActionRecognitionModule.py
@@ -14,14 +14,16 @@ creatingClusters = False
 creatingActivitySequence = True
 numberOfClubsters = 5
 activity = 'WavingRightHand'
-
+activitySequence =[]
 
 #LOAD CLOSTERS
 if training and creatingActivitySequence:
 	clusters = ActivityFeatureComputation.loadClusters()
 	print 'clusters: \n',clusters
 
-# ActivityFeatureComputation.createActivitySequence(clusters, [[-0.009005660612280015, -1.8705235122661397], [-0.04563693655945724, -0.998958092224828], [1.1430441925446853, 0.6327943850946958], [-1.2159814718985813, 0.4932803845028426], [1.2924588710434584, 1.5129025026361729], [-1.0244124795520126, 1.222698061324344], [0.5990121106562336, 3.593948226694114], [-0.5377174131316365, 3.6293186299806006], [0.6880202229902418, 4.9371415710844095], [-0.6293457509324764, 5.002503521509857]] )
+
+
+# ActivityFeatureComputation.createPostureLabel(clusters, [[-0.009005660612280015, -1.8705235122661397], [-0.04563693655945724, -0.998958092224828], [1.1430441925446853, 0.6327943850946958], [-1.2159814718985813, 0.4932803845028426], [1.2924588710434584, 1.5129025026361729], [-1.0244124795520126, 1.222698061324344], [0.5990121106562336, 3.593948226694114], [-0.5377174131316365, 3.6293186299806006], [0.6880202229902418, 4.9371415710844095], [-0.6293457509324764, 5.002503521509857]] )
 
 def actionRecognition(skeletons, dispInfo):
 	skeleton = skeletons[0]
@@ -36,8 +38,14 @@ def actionRecognition(skeletons, dispInfo):
 	#CREATE ACTIVITY SEQUENCE
 	elif training and creatingActivitySequence:
 		# print 'f',f
-		activitySequence = ActivityFeatureComputation.createActivitySequence(clusters, f)
-		print 'Activity sequence: \n',activitySequence
+		postureLabel = ActivityFeatureComputation.createPostureLabel(clusters, f)
+		print 'Posture Label: \n', postureLabel
+
+		if postureLabel !=-1:
+			global activitySequence
+			activitySequence = ActivityFeatureComputation.createActivitySequence(activitySequence,postureLabel)
+
+
 
 
 #CREATE CLUSTERS OF AN ACTIVITY
--- actionRecognition/ActivityFeatureComputation.py
+++ actionRecognition/ActivityFeatureComputation.py
@@ -12,12 +12,12 @@ def loadClusters():
 	fo.close()
 	return clusters
 
-def createActivitySequence(clusters, Posturefeature):
+def createPostureLabel(clusters, Posturefeature):
 	try:
 		X = np.array(clusters)
 		kmeans = KMeans(n_clusters=len(clusters), random_state=0).fit(X)
 		Posturefeature2D = np.array([ Posturefeature ])
-		print 'Posturefeature2D 1:\n',Posturefeature2D
+		# print 'Posturefeature2D 1:\n',Posturefeature2D
 		nsamples, nx, ny = Posturefeature2D.shape
 		Posturefeature2D = Posturefeature2D.reshape((nsamples, nx*ny)).tolist()[0]	
 		postureLabel = kmeans.predict([Posturefeature2D]).tolist()[0]
@@ -31,5 +31,20 @@ def createActivitySequence(clusters, Posturefeature):
 	# print(kmeans.predict(kmeans.cluster_centers_).tolist())
 	return postureLabel
 
+def createActivitySequence(activitySequence, postureLabel):
+	print 'activitySequence: ', activitySequence
+	if len(activitySequence)==0:
+		sequence =[postureLabel]
+	elif len(activitySequence)<5 and postureLabel!=activitySequence[len(activitySequence)-1]:
+		sequence = activitySequence + [postureLabel]
+	elif len(activitySequence)==5:
+		print 'Activity Sequence', activitySequence
+		sequence=[]
+	else:
+		sequence = activitySequence
 
-# createActivitySequence(loadClusters(), [[-0.009005660612280015, -1.8705235122661397], [-0.04563693655945724, -0.998958092224828], [1.1430441925446853, 0.6327943850946958], [-1.2159814718985813, 0.4932803845028426], [1.2924588710434584, 1.5129025026361729], [-1.0244124795520126, 1.222698061324344], [0.5990121106562336, 3.593948226694114], [-0.5377174131316365, 3.6293186299806006], [0.6880202229902418, 4.9371415710844095], [-0.6293457509324764, 5.002503521509857]] )
+	return sequence
+
+
+
+# createPostureLabel(loadClusters(), [[-0.009005660612280015, -1.8705235122661397], [-0.04563693655945724, -0.998958092224828], [1.1430441925446853, 0.6327943850946958], [-1.2159814718985813, 0.4932803845028426], [1.2924588710434584, 1.5129025026361729], [-1.0244124795520126, 1.222698061324344], [0.5990121106562336, 3.593948226694114], [-0.5377174131316365, 3.6293186299806006], [0.6880202229902418, 4.9371415710844095], [-0.6293457509324764, 5.002503521509857]] )
